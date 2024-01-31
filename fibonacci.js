ee8b994daee15f12cc52bdb2f0c0c5e1c45d90f5
Oliver Alvarado <153557532+oliveroalvarado@users.noreply.github.com>
2024-01-31 13:25:53 -0800

    new

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-  return 0;
+
+  let preSet = 0; // first always going to be 0
+  let preSet1 = 1; // first term always going to be 1
+  let sum = 0; // this witll ne combination of preSet + preSet1
+
+  //
+for (let i = 2; i <= num; i++) {
+  sum = (preSet + preSet1);
+  preSet = preSet1;
+  preSet1 = sum;
 }
+return preSet1
+}
+
+console.log(fibonacci(11)); 
 
 module.exports = fibonacci;
